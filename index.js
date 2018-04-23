0c4957b561cff38adac338d7caa04ca80da95e21
wrongeric <33172384+wrongeric@users.noreply.github.com>
2018-04-23 16:58:15 -0700

    added initial passport and express setup for google oauth

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 
-const express = require('express')
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({hey: 'man'});
-});
+passport.use(new GoogleStrategy());
+
+// app.get('/', (req, res) => {
+//     res.send({hey: 'man'});
+// });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
