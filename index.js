5a763d399c3fcdeec8ebec092fb41f968e82501f
wrongeric <33172384+wrongeric@users.noreply.github.com>
2018-04-23 12:32:59 -0700

    changed greeting

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const express = require('express')
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'there'});
+    res.send({hey: 'man'});
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+//https://pacific-dusk-41353.herokuapp.com/ | https://git.heroku.com/pacific-dusk-41353.git
