ff3e095d6f2ad11d1f67ff4a57fc54124ea82002
wrongeric <33172384+wrongeric@users.noreply.github.com>
2018-04-23 23:02:35 -0700

    added keys.js to config folder

--- index.js
+++ index.js
@@ -7,13 +7,6 @@ const app = express();
 
 passport.use(new GoogleStrategy());
 
-// app.get('/', (req, res) => {
-//     res.send({hey: 'man'});
-// });
-
-//clientID    115335116427-i1h88rejotjd1hdr4pe6r0mdg3g57tvh.apps.googleusercontent.com
-
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
