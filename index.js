093515ca677f78c0b325bd546e86311a1705f29a
wrongeric <33172384+wrongeric@users.noreply.github.com>
2018-04-23 22:55:56 -0700

    signed up for google+ and beginning google-oauth-20

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ passport.use(new GoogleStrategy());
 //     res.send({hey: 'man'});
 // });
 
+//clientID    115335116427-i1h88rejotjd1hdr4pe6r0mdg3g57tvh.apps.googleusercontent.com
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
