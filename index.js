fb604872accec3e2000b29accdcb1e9ab9af5981
wrongeric <33172384+wrongeric@users.noreply.github.com>
2018-04-26 23:50:32 -0700

    added route for auth google callback

--- index.js
+++ index.js
@@ -17,12 +17,16 @@ passport.use(
   )
 );
 
+//https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth%2Fgoogle%2Fcallback&scope=profile%20email&client_id=115335116427-i1h88rejotjd1hdr4pe6r0mdg3g57tvh.apps.googleusercontent.com
+//error message 400 Error: redirect_uri_mismatch
 app.get(
   "/auth/google",
   passport.authenticate("google", {
     scope: ["profile", "email"]
   })
 );
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
