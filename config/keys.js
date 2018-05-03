0c8aa38e86817f282a1f2aed6863f77864c2fe19
wrongeric <33172384+wrongeric@users.noreply.github.com>
2018-05-02 22:43:53 -0700

    fixed typo in keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 //keys.js
 if (process.env.NODE_ENV === "production") {
   //we are in production - return the prod set of keys
-  module.exportsrequire("./prod");
+  module.exports = require("./prod");
 } else {
   //we are in development - return the dev keys
   module.exports = require("./dev");
