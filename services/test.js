9534c35bbf6cc7119009a1bcde83432ce43ab9bd
wrongeric <33172384+wrongeric@users.noreply.github.com>
2018-05-04 16:04:17 -0700

    added async/await test file

---
+++ services/test.js
@@ -0,0 +1,20 @@
+// write a function to retrieve a blob of json
+// make an ajax request!  Use the 'fetch' function
+//  https://rallycoding.herokuapp.com/api/music_albums
+
+//ES 2015
+// function fetchAlbums() {
+//   fetch("https://rallycoding.herokuapp.com/api/music_albums")
+//     .then(res => res.json())
+//     .then(json => console.log(json));
+// }
+
+//async/await ES2017
+async function fetchAlbums() {
+  const res = await fetch("https://rallycoding.herokuapp.com/api/music_albums");
+  const json = await res.json();
+
+  console.log(json);
+}
+
+fetchAlbums();
