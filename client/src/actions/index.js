5abe766f50b2270e03a0b1ccce0b431c7e1754c6
wrongeric <33172384+wrongeric@users.noreply.github.com>
2018-05-05 21:35:18 -0700

    added Header Content based on authentication stsatus

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,13 +1,19 @@
 import axios from "axios";
 import { FETCH_USER } from "./types";
 
-export const fetchUser = () => {
-  return function(dispatch) {
-    axios
-      .get("/api/current_user")
-      .then(res => dispatch({ type: FETCH_USER, payload: res }));
-  };
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get("/api/current_user");
+
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
+
+// export const fetchUser = () => {
+//   return function(dispatch) {
+//     axios
+//       .get("/api/current_user")
+//       .then(res => dispatch({ type: FETCH_USER, payload: res }));
+//   };
+// };
 
 // const fetchUser = () => {
 //     const request = axios.get("/api/current_user");
