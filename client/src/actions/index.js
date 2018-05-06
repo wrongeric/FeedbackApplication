1d4f36f2e0a7d19612f000b8024caee7f7048960
wrongeric <33172384+wrongeric@users.noreply.github.com>
2018-05-05 20:31:20 -0700

    added reduxThunk and finished fetch_user api request

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,19 @@
 import axios from "axios";
 import { FETCH_USER } from "./types";
 
-const fetchUser = () => {
-  axios.get("/api/current_user");
+export const fetchUser = () => {
+  return function(dispatch) {
+    axios
+      .get("/api/current_user")
+      .then(res => dispatch({ type: FETCH_USER, payload: res }));
+  };
 };
+
+// const fetchUser = () => {
+//     const request = axios.get("/api/current_user");
+
+//     return {
+//         type: FETCH_USER,
+//         payload: request
+//     };
+// };
