81ee3558f1ce4b4255286806dadbaf4d78ab6bf9
wrongeric <33172384+wrongeric@users.noreply.github.com>
2018-05-06 23:27:43 -0700

    added redirect to landing page on logout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -17,7 +17,7 @@ class Header extends Component {
       default:
         return (
           <li>
-            <a>Logout</a>
+            <a href="/api/logout">Logout</a>
           </li>
         );
     }
