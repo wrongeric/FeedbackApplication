04182c8cea61f49c1dbb84029071711d8fb7935f
wrongeric <33172384+wrongeric@users.noreply.github.com>
2018-05-07 13:46:23 -0700

    added stripe checkout component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import Payments from "./Payments";
 
 class Header extends Component {
   renderContent() {
@@ -16,11 +17,14 @@ class Header extends Component {
         );
 
       default:
-        return (
+        return [
+          <li>
+            <Payments />
+          </li>,
           <li>
             <a href="/api/logout">Logout</a>
           </li>
-        );
+        ];
     }
   }
 
