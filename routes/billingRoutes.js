adc0527cdcd2ce9d93605c92fa1f061bb6be9fae
wrongeric <33172384+wrongeric@users.noreply.github.com>
2018-05-13 14:48:21 -0700

    added aysnc/await syntax for the promise on the backend charge

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,14 +1,15 @@
 const keys = require("../config/keys");
 const stripe = require("stripe")(keys.stripeSecretKey);
-const passport = require("passort");
+const passport = require("passport");
 
 module.exports = app => {
-  app.post("/api/stripe", (req, res) => {
-    stripe.charges.create({
+  app.post("/api/stripe", async (req, res) => {
+    const charge = await stripe.charges.create({
       amount: 500,
       currency: "usd",
       description: "$5 for 5 Credits",
       source: req.body.id
     });
+    console.log(charge);
   });
 };
