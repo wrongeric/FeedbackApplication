0c1c1adc96f5afdcda9287e9255def098d551906
wrongeric <33172384+wrongeric@users.noreply.github.com>
2018-05-13 14:57:17 -0700

    added error handler if user is not logged in but tries to add credits

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -4,12 +4,19 @@ const passport = require("passport");
 
 module.exports = app => {
   app.post("/api/stripe", async (req, res) => {
+    if (!req.user) {
+      return res.status(401).send({ error: "You must be logged in!" });
+    }
     const charge = await stripe.charges.create({
       amount: 500,
       currency: "usd",
       description: "$5 for 5 Credits",
       source: req.body.id
     });
-    console.log(charge);
+
+    req.user.credits += 5;
+    const user = await req.user.save();
+
+    res.send(user);
   });
 };
