60f395cca6fff0f7ee531ebed79022ef2882ed90
wrongeric <33172384+wrongeric@users.noreply.github.com>
2018-05-16 23:26:39 -0700

    added in production routes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const keys = require("./config/keys");
 require("./models/user"); //order of require statements can result in errors
 require("./services/passport");
 
-// const authRoutes = require('./routes/authROutes');
+// const authRoutes = require('./routes/authRoutes');
 
 mongoose.connect(keys.mongoURI);
 
@@ -30,6 +30,16 @@ require("./routes/billingRoutes")(app);
 
 //https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth%2Fgoogle%2Fcallback&scope=profile%20email&client_id=115335116427-i1h88rejotjd1hdr4pe6r0mdg3g57tvh.apps.googleusercontent.com
 //error message 400 Error: redirect_uri_mismatch
+
+if (process.env.NODE_ENV === "production") {
+  //Express will serve up production assets like our main.js or main.css files
+  app.use(express.static("client/build"));
+  //Express will serve up the index.html file if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
