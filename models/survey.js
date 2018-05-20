412a61de3f3061fd7102698dab6905a2fb6f440e
wrongeric <33172384+wrongeric@users.noreply.github.com>
2018-05-20 16:55:24 -0700

    added subdocument record

--- models/survey.js
+++ models/survey.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const RecipientSchema = require("./recipient");
 
 const surveySchema = new Schema({
   title: String,
   body: String,
   subject: String,
-  recipients: [String],
+  recipients: [RecipientSchema],
   Yes: { type: Number, default: 0 },
   No: { type: Number, default: 0 }
 });
